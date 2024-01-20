94a2f52e9b4ba4e4d40cd76e885d8f0173482102
VoxTerrain <154274770+VoxTerrain@users.noreply.github.com>
2024-01-20 17:18:31 +0200

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
